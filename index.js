c71a9413aae5d61113ac48574157e96f34f7396b
Pedro Correa <pedroorez@gmail.com>
2016-02-10 15:24:52 -0200

    Add LambdaURI path builder.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var apigateway = require('./lib/apigateway.js')
+var dataProcess = require('./lib/dataProcess.js')
 
 apigateway.purgeApi()
+  .then(() => dataProcess.buildLambdaURI('arn:aws:lambda:us-east-1:522617982767:function:dev-ManageProjectsId-GET'))
   .then((data) => console.log(data))
   .catch((err) => console.log(err.stack))
