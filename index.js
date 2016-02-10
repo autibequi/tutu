244c2a4a8aa5d6e3a2987b0d373037d6db622bdb
Pedro Correa <pedroorez@gmail.com>
2016-02-10 15:51:23 -0200

    Add deploy resources deployment step.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 
 var apigateway = require('./lib/apigateway.js')
+var deploy = require('./lib/deploy.js')
 var dataProcess = require('./lib/dataProcess.js')
 var aux = require('./lib/aux.js')
 var SETTINGS = require('./lib/settings.js').constants
@@ -11,5 +12,6 @@ apigateway.purgeApi()
   .then(() => dataProcess.buildLambdaURI('arn:aws:lambda:us-east-1:522617982767:function:dev-ManageProjectsId-GET'))
   .then(() => JSON.parse(fs.readFileSync(endpointsFilePath, 'utf8')))
   .then(dataProcess.prepareRootResources)
+  .then(deploy.deployResources)
   .then((data) => console.log(data))
   .catch((err) => console.log(err.stack))
