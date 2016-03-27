329d12998c25417a059b853164c04ed06b16bae8
Pedro Correa <pedroorez@gmail.com>
2016-03-27 02:04:23 -0300

    Minor rework on index.js

--- bin/index.js
+++ bin/index.js
@@ -3,30 +3,28 @@
 // --------------------------------------
 var lambda = require('../lib/lambda.js')
 var apigateway = require('../lib/apigateway.js')
-var deploy = require('../lib/deploy.js')
+var deployer = require('../lib/deploy.js')
 var dataProcess = require('../lib/dataProcess.js')
 var packagers = require('../lib/packagers.js')
 var cloudwatchevents = require('../lib/cloudwatchevents.js')
 
 // Deploy Standalone Lambdas
-exports.deployStandaloneLambdas = () => {
-  return cloudwatchevents.purgeRules()
+exports.deployStandaloneLambdas = () =>
+  cloudwatchevents.purgeRules()
     .then(cloudwatchevents.loadStandaloneFile)
     .then(cloudwatchevents.deployStandaloneLambdas)
-}
 
 // Deploy API
-exports.deploy = () => {
-  return packagers.buildUnifiedLambdaFunction
+exports.deploy = () =>
+  packagers.buildUnifiedLambdaFunction
     .then(lambda.deployLambdaFunction)
     .then(lambda.addResourceBasedPermissionToAPI)
     .then(dataProcess.buildRootResources)
-    .then(deploy.deployResources)
+    .then(deployer.deployResources)
     .then(apigateway.createDeployment)
-}
+
 
 // Purge APIGateway and API
-exports.purgeAndDeploy = () => {
-  return apigateway.purgeApi
+exports.purgeAndDeploy = () =>
+  apigateway.purgeApi
     .then(exports.deploy)
-}
