d6ef1a27d64d3613eb2299e9e7fcbebdd875bc08
Pedro Correa <pedroorez@gmail.com>
2016-04-19 23:53:55 -0300

    Adds first test :D

---
+++ test/dataProcess.js
@@ -0,0 +1,23 @@
+var assert = require('chai').assert;
+var dataProcess = require("../lib/dataProcess.js");
+
+
+
+describe('APIGateway method JSON setup building', function() {
+  describe('buildOptionsResponseParameters', function () {
+    it('Should loop all an array of methods and create the right JSON', function () {
+      var awsner = {
+        "method.response.header.Access-Control-Allow-Origin": "\'*\'",
+        "method.response.header.Access-Control-Allow-Headers": "\'Content-Type,X-Amz-Date,Authorization,x-Authorization\'",
+        "method.response.header.Access-Control-Allow-Methods": "\'GET,POST\'"
+      }
+
+      var resource = {
+        "Methods": ["GET", "POST"]
+      }
+
+      var responseParams = dataProcess.buildOptionsResponseParameters(resource)
+      assert.equal(JSON.stringify(awsner), JSON.stringify(responseParams));
+    });
+  });
+});
