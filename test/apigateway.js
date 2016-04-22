da4da3b2b78f5d47b14827899f977569ee4ccaa9
Pedro Correa <pedroorez@gmail.com>
2016-04-22 20:58:44 -0300

    Improve Retrier Tests.

--- test/apigateway.js
+++ test/apigateway.js
@@ -4,30 +4,35 @@ var SETTINGS = require("../lib/settings.js")
 
 SETTINGS.TIMEOUT = 1
 
-describe('APIGateway retrier', function() {
-  describe('retier', function () {
-    it('should return a promise', function (done) {
+describe('APIGateway', function() {
+  describe('Retrier Function', function () {
+    it('Should retry a function until the limit is reached', function (done) {
 
       var params = {
         value: "test"
       }
 
-      return apigateway.okay(params)
+      return apigateway.retry(params)
+          .then((data) => {
+            done('It shouldnt return a non rejected promise')
+          })
           .catch((err) => {
-            console.log(err)
+            assert.equal(SETTINGS.API_RETRY_LIMIT - 1, err.retryCount);
             done()
           })
     });
 
 
-    it('should return a reject', function (done) {
+    it('Should reject a error code different than specified', function (done) {
       var params = {
         value: "test"
       }
 
       return apigateway.fail(params)
+          .then((data) => {
+            done('Shouldnt resolve()')
+          })
           .catch((err) => {
-            console.log(err)
             done()
           })
     });
